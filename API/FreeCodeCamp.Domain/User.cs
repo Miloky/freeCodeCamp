bb635c813ca2a47c6614456ebc57725370f603ce
Miloky <monmorti@gmail.com>
2020-09-13 22:51:41 +0300

    domain

---
+++ API/FreeCodeCamp.Domain/User.cs
@@ -0,0 +1,11 @@
+namespace FreeCodeCamp.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string NormalizedUsername { get; set; }
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+    }
+}
