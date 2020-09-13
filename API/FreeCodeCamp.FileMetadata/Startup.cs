b31777e51e6d08cd7d119056f4b367aa0c100c80
Miloky <monmorti@gmail.com>
2020-09-13 22:52:44 +0300

    persitence

--- API/FreeCodeCamp.FileMetadata/Startup.cs
+++ API/FreeCodeCamp.FileMetadata/Startup.cs
@@ -10,6 +10,7 @@ namespace FreeCodeCamp.FileMetadata
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddPersistence();
             // Register the Swagger services
             services.AddSwaggerDocument();
         }
@@ -19,7 +20,7 @@ namespace FreeCodeCamp.FileMetadata
             // Register the Swagger generator and the Swagger UI middlewares
             app.UseOpenApi();
             app.UseSwaggerUi3();
-
+            
             app.UseRouting();
             app.UseEndpoints(options =>
             {
---
+++ API/FreeCodeCamp.Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using FreeCodeCamp.Domain;
+
+namespace FreeCodeCamp.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
