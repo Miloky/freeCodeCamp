7a91557dd3286f0fcf17372df884948e5acb5fd6
Miloky <monmorti@gmail.com>
2020-09-20 21:05:06 +0300

    todo list project

--- API/FreeCodeCamp.FileMetadata/ClientApp/src/pages/root.js
+++ API/FreeCodeCamp.FileMetadata/ClientApp/src/pages/root.js
@@ -9,8 +9,12 @@ const Root = () => {
     <Header/>
     <Container>
       <Grid container spacing={3} >
-        <Grid item xs={12} sm={6} md={4}><Project url='/file-metadata' /></Grid>
-        {[1, 2, 3, 4, 5, 6].map(index => <Grid item xs={12} sm={6} md={4} key={index}><Project /></Grid>)}
+        <Grid item xs={12} sm={6} md={4}>
+          <Project url='/file-metadata' logo='/assets/images/file-metadata-logo.png' header='File Metadata API' />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Project url='/' header='TODO List' logo='https://indyme.com/wp-content/uploads/2016/04/coming-soon.png' />
+        </Grid>
       </Grid>
     </Container>
   </>;
