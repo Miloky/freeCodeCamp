da071b6472979458e3272aecbab65702b9e9034f
Miloky <monmorti@gmail.com>
2020-09-20 21:04:26 +0300

    project technologies

--- API/FreeCodeCamp.FileMetadata/ClientApp/src/components/project/project.js
+++ API/FreeCodeCamp.FileMetadata/ClientApp/src/components/project/project.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -6,6 +6,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { useHistory } from 'react-router-dom';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx';
+import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles({
   root: {
@@ -13,35 +18,76 @@ const useStyles = makeStyles({
     maxWidth: 345
   },
   media: {
-    height: 140
+    height: 140,
+    backgroundPosition: 'initial'
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  header:{
+    marginBottom: 0
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  pad: {
+    marginBottom: '5px',
+    marginRight: '5px'
   }
 });
 
 const MediaCard = (props) => {
   const classes = useStyles();
   const history = useHistory();
+  const [expanded, setExpanded] = useState(false);
 
   const handleNavigate = () => {
     history.push(props.url);
   };
+
+  const handleExpandClick = (event) =>{
+    event.preventDefault();
+    event.stopPropagation();
+    setExpanded(!expanded);
+  }
 
   return (
     <Card className={classes.root}>
       <CardActionArea onClick={handleNavigate}>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          image={props.logo}
+          title="logo"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
+          <div className={classes.headerContainer}>
+            <Typography gutterBottom variant="h5" component="h2" className={classes.header}>
+              {props.header}
+            </Typography>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </div>
         </CardContent>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+              <Chip className={classes.pad} label="asp.net core" onClick={()=>{}} />
+              <Chip className={classes.pad} label="react" onClick={()=>{}} />
+              <Chip className={classes.pad} label="axios" onClick={()=>{}} />
+              <Chip className={classes.pad} label="material-ui" onClick={()=>{}} />
+              <Chip className={classes.pad} label="rxjs" onClick={()=>{}} />
+          </CardContent>
+        </Collapse>
       </CardActionArea>
     </Card>
   );
